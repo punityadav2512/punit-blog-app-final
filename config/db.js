2338db733f206827a5ef88caf1423b1e876034bc
punityadav2512 <b520040@iiit-bh.ac.in>
2022-01-02 10:00:39 +0530

    sdlkfskl

--- config/db.js
+++ config/db.js
@@ -5,8 +5,7 @@ const connectToDB = async () => {
         await mongoose.connect(process.env.APP_DB_CONNECTION, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
-            useMongoClient: true
+
         })
         console.log('Connected to DB!')
     } catch (error) {
